35f15260f6e0f6bb55bd14e0473e80d81f4bb048
bmix1 <bmix@umd.edu>
2017-05-15 23:31:52 -0400

    Add files via upload

---
+++ sudoku.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 15 23:21:59 2017
+
+@author: bmix
+"""
+
+def createGrid: # this function creates the grid for the puzzle
+
+
+
+def addValues: # this function adds values to the grid
+
+
+
+
+def solvePuzzle:
+    
+    
